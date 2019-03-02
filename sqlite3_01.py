889617217a40fc01a2c07a8fc7b999df9001dced
gaurashish99 <gaurashish99@gmail.com>
2019-03-02 23:27:25 +0530

    learning database using sqlite2

--- sqlite3_01.py
+++ sqlite3_01.py
@@ -3,17 +3,26 @@
 import sqlite3
 
 def main():
-		con =sqlite3.connect('test.db')
-		cur=con.cursor()
-		print("Details: ")
-		cur.execute("Drop table if exists pets")
-		cur.execute("Create table pets (ID INT, Name TEXT, Price INT)")
-		cur.execute("Insert into pets values(1,'cat',400)")
-		con.commit()
-		cur.execute("select * from pets")
-		data=cur.fetchall()
-		for row in data:
-			print (row)
+		try:
+			con =sqlite3.connect('test.db')
+			cur=con.cursor()
+			print("Details: ")
+			cur.execute("Drop table if exists pets")
+			cur.execute("Create table pets (ID INT, Name TEXT, Price INT)")
+			cur.execute("Insert into pets values(1,'cat',400)")
+			con.commit()
+			cur.execute("select * from pets")
+			data=cur.fetchall()
+			for row in data:
+				print (row)
+
+		except:
+			if con:
+				con.rollback()
+				print ("Error: "+str(e))
+		finally:
+			con.close()
+			print("Closing")
 
 
 
